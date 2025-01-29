e7ed521f90a04b2df65669c33ab38e427a0583c5
ser1nlu <serinlu2201@gmail.com>
2025-01-29 01:05:46 -0500

    feat: update cookie settings for enhanced security and compatibility

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -229,9 +229,9 @@ export const login = async (req, res) => {
                 const token = generateToken(user._id, user.role);
                 res.cookie("token", token, {
                     httpOnly: true,
-                    secure: 'false',  
-                    maxAge: 24 * 60 * 60 * 1000,
-                    sameSite: 'None',
+                    secure: true,
+                    maxAge: 86400000,
+                    sameSite: 'none',
                 });
 
                 return res.status(200).json({ token, user });
